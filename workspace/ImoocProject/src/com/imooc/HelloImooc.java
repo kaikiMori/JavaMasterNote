4d26102671b97ab8070b682038b0b75a4c37fcfc
kakaiMori <kakaijapan@gmail.com>
2023-09-07 12:46:07 +0900

    try on eclipse

---
+++ workspace/ImoocProject/src/com/imooc/HelloImooc.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+public class HelloImooc {
+
+	public static void main(String[] args) {
+		System.out.println("hello, world!");
+		char c = '\u005d';
+		System.out.println("c =" +c);
+
+	}
+
+}
